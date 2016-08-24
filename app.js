15bf57c20344d6edaad314c519c9f42706b3df55
JeremyVe <jepschx@gmail.com>
2016-08-24 13:25:25 +0800

    added quotes and form validation

--- app.js
+++ app.js
@@ -55,3 +55,52 @@ directiveMadness.directive("colorize", function() {
 		link: linkCallback
 	}
 })
+
+directiveMadness.controller("QuotesCtrl", 
+	["$scope", 
+	function($scope) {
+		$scope.quotes = [];
+
+		$scope.formData = {};
+
+		$scope.submit = function(formValid, formQuote) {
+
+			if (formValid) {
+
+				var newQuote = {};
+				newQuote.message = $scope.formData.message;
+				newQuote.author = $scope.formData.author;
+				$scope.quotes.push(newQuote);
+
+
+				$scope.formData.message = '';
+				$scope.formData.author = '';
+
+				formQuote.$setPristine();
+				formQuote.$setUntouched();
+			}
+		}
+
+		$scope.deleteQuote = function(index) {
+			$scope.quotes.splice(index, 1);
+		}
+
+}])
+
+directiveMadness.directive("quoteForm", function() {
+	
+	return {
+		templateUrl: "directives/quoteForm.html",
+		restrict: "E",
+		scope: true
+	}
+})
+
+directiveMadness.directive("quotesIndex", function() {
+
+	return {
+		templateUrl: "directives/quotesIndex.html",
+		restrict: "E",
+		scope: true
+	}
+})
